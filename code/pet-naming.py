ab9e4f29bdb5a33b243c366a1bcbc5dc0b5122c8
Derek Graham <deejaygraham@gmail.com>
2017-10-18 12:08:16 +0100

    some code

---
+++ code/pet-naming.py
@@ -0,0 +1,7 @@
+import random
+
+names = ['Rover', 'Spot', 'Inca', 'Craig', 'Peter', 'Linda', 'Shirley', 'Chris']
+suggestion = random.choice(names)
+
+print("I recommend you call your dog " + suggestion)
+
