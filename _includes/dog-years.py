177d64dcb266bcaf38935baa94250e6e0c2cc2e0
Derek Graham <deejaygraham@gmail.com>
2018-02-25 21:40:11 +0000

    Create dog-years.py

---
+++ _includes/dog-years.py
@@ -0,0 +1,8 @@
+
+from include top level
+
+name = input('What is your name ?')
+age = input('How old are you ?')
+age = int(age)
+
+print("If " + name + " was a dog, they'd be " + str(age * 7) + " years old")
