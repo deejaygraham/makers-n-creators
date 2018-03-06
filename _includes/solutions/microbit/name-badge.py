a64b018de61ee88cd5342df6be6cf8c38bc58862
Derek Graham <deejaygraham@gmail.com>
2018-03-06 12:22:00 +0000

    Create name-badge.py

---
+++ _includes/solutions/microbit/name-badge.py
@@ -0,0 +1,10 @@
+from microbit import *
+
+greeting = 'hi!'
+introduction = 'my name is '
+name = 'Sarah'
+
+while True:
+  display.scroll(greeting)
+  display.scroll(introduction)
+  display.scroll(name)
