d6a57f4e501db58a020bb8bbbbe3baf0ef72953b
Derek Graham <deejaygraham@gmail.com>
2018-03-06 12:37:45 +0000

    Update name-badge.py

--- _includes/solutions/microbit/name-badge.py
+++ _includes/solutions/microbit/name-badge.py
@@ -2,9 +2,9 @@ from microbit import *
 
 greeting = 'hi!'
 introduction = 'my name is '
-name = 'Sarah'
+my_name = 'Sarah'
 
 while True:
   display.scroll(greeting)
   display.scroll(introduction)
-  display.scroll(name)
+  display.scroll(my_name)
