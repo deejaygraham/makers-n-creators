1de400a3a11c8d7f46d73bc75867e3e6da758979
Derek Graham <deejaygraham@gmail.com>
2019-12-16 08:00:54 +0000

    Update show-hide-solution.js

--- js/show-hide-solution.js
+++ js/show-hide-solution.js
@@ -1,15 +1,15 @@
 window.onload = function() {
     var x = document.getElementsByClassName("language-python")[0];
-    x.style.display = 'none';
+    x.style.display = "none";
   };
 
 function show_hide_solution() {
     var x = document.getElementsByClassName("language-python")[0];
     if (x.style.display === "none") {
       x.style.display = "block";
-      x.value = 'Hide Solution'
+      x.value = "Hide Solution";
     } else {
       x.style.display = "none";
-      x.value = 'Show Solution'
+      x.value = "Show Solution";
     }
   }
