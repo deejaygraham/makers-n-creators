c9105cd75ebd7807ea517199ed0c90d4d97ba284
Derek Graham <deejaygraham@gmail.com>
2019-12-16 07:56:33 +0000

    hide on page load

--- js/show-hide-solution.js
+++ js/show-hide-solution.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+    document.getElementById('b-navbar-fg').style.display = 'none';
+    var x = document.getElementsByClassName("language-python")[0];
+    x.style.display = 'none';
+  };
+
 function show_hide_solution() {
     var x = document.getElementsByClassName("language-python")[0];
     if (x.style.display === "none") {
