87e206fa331c0dbe6c421fae3794523d09c70fb9
Derek Graham <deejaygraham@gmail.com>
2019-12-16 07:59:11 +0000

    button

--- js/show-hide-solution.js
+++ js/show-hide-solution.js
@@ -8,7 +8,9 @@ function show_hide_solution() {
     var x = document.getElementsByClassName("language-python")[0];
     if (x.style.display === "none") {
       x.style.display = "block";
+      x.value = 'Hide Solution'
     } else {
       x.style.display = "none";
+      x.value = 'Show Solution'
     }
   }
